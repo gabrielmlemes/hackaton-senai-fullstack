6fbbee18e0d1a4e8f0f9b90581fb83e2d9407160
Gabriel Lemes <gabrielmlemes@gmail.com>
2025-12-03 11:46:30 -0300

    chore: change app title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./_components/footer"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "FSW BARBER",
+  title: "Hackaton Senai Fullstack",
   description: "A sua barbearia!",
 }
 
