f1840c0588582833f3127d20a10db756ffedd51f
Gabriel Lemes <gabrielmlemes@gmail.com>
2025-12-03 11:22:14 -0300

    build: clear unused files to ensure deployment

--- frontend/app/_actions/delete-booking.ts
+++ frontend/app/_actions/delete-booking.ts
@@ -1,16 +1,6 @@
 "use server"
 
 import { revalidatePath } from "next/cache"
-// import { db } from "../_lib/prisma"
-
-// export const deleteBooking = async (bookingId: string) => {
-//   await db.booking.delete({
-//     where: {
-//       id: bookingId,
-//     },
-//   })
-//   revalidatePath("/bookings")
-// }
 
 interface DeleteBookingParams {
   bookingId: string
